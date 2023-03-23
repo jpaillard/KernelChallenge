525d171461825ec7046ad9a1a23b03371d63a0ff
jpaillard <joseph.paillard@etu.minesparis.psl.eu>
2023-03-23 17:17:55 +0100

    parallelize kfold split

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
+from joblib import Parallel
+from joblib import delayed
 from sklearn.metrics import f1_score
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.model_selection import StratifiedKFold
 
+from KernelChallenge.classifiers import KernelSVC
 from KernelChallenge.kernels import WesifeilerLehmanKernel
-from KernelChallenge.kernels import gramMatrix
 from KernelChallenge.preprocessing import WL_preprocess
 
 
@@ -16,8 +18,19 @@ def parse_args():
     parser.add_argument('--data_path', type=str, default='data/')
     parser.add_argument('--n', type=int, default=1000)
     parser.add_argument('--h_iter', type=int, default=2)
-    parser.add_argument('--c', type=int, default=1000)
+    parser.add_argument('--c', type=float, default=1000)
     return parser.parse_args()
+
+
+def fit_predict_one(Gn, labels, train_index, test_index):
+    X_train, X_test = Gn[train_index], Gn[test_index]
+    y_train, y_test = labels[train_index], labels[test_index]
+    WLK = WesifeilerLehmanKernel(h_iter=args.h_iter)
+
+    clf = KernelSVC(C=args.c, kernel=WLK)
+    clf.fit(X_train, y_train)
+    f1 = f1_score(y_test, clf.predict(X_test))
+    return f1
 
 
 if __name__ == '__main__':
@@ -26,24 +39,27 @@ if __name__ == '__main__':
     data_path = Path(args.data_path)
     train_data = pd.read_pickle(data_path / 'training_data.pkl')
     labels = pd.read_pickle(data_path / 'training_labels.pkl')
+    labels = 2 * labels - 1
 
-    Gn = WL_preprocess(train_data[:args.n])
-    print(len(Gn))
-    X_train, X_test, y_train, y_test = train_test_split(
-        Gn,
-        labels[:args.n],
-        test_size=0.2,
-        random_state=0,
-        stratify=labels[:args.n]
-    )
+    # Keep only the largest connected component
+    # train_data = preprocess(train_data, preprocess_f0=preprocess_one)
 
-    WLK = WesifeilerLehmanKernel(h_iter=args.h_iter)
-    feat_train = WLK.fit_subtree(X_train)
-    feat_test = WLK.predict(X_test)
+    Gn = np.array(WL_preprocess(train_data[:args.n]))
 
-    K_train = gramMatrix(feat_train, feat_train)
-    K_test = gramMatrix(feat_test, feat_train)
-    clf = SVC(C=args.c, kernel='precomputed')
-    clf.fit(K_train, y_train)
-    print("\nF1 score on {:d} samples using WL kernel :  {:.2f}".format(
-        args.n, f1_score(y_test, clf.predict(K_test))))
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+    skf.get_n_splits(Gn, labels[:args.n])
+
+    scores = Parallel(n_jobs=5)(delayed(
+        fit_predict_one)(Gn,
+                         labels,
+                         train_index,
+                         test_index)
+        for train_index, test_index in skf.split(Gn, labels[:args.n]))
+
+    print(
+        "\nF1 score on {:d} samples using WL kernel : {:.2f}+/-{:.2f}".format(
+            args.n,
+            np.mean(scores),
+            np.std(scores)
+        ))
+    print(scores)

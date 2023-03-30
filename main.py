66eb0a8513b7717b0cbd1fae46af76aa0f1791f2
jpaillard <joseph.paillard@etu.minesparis.psl.eu>
2023-03-30 18:36:26 +0200

    update main

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# import cProfile
 from argparse import ArgumentParser
 from pathlib import Path
+from time import time
 
 import numpy as np
 import pandas as pd
-from joblib import Parallel
-from joblib import delayed
+# from joblib import Parallel
+# from joblib import delayed
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
 
 from KernelChallenge.classifiers import KernelSVC
 from KernelChallenge.kernels import WesifeilerLehmanKernel
+# from KernelChallenge.kernels import gramMatrix
 from KernelChallenge.preprocessing import WL_preprocess
 
 # from sklearn.svm import SVC
@@ -48,6 +51,7 @@ if __name__ == '__main__':
     labels = pd.read_pickle(data_path / 'training_labels.pkl')
     labels = 2 * labels - 1
 
+    np.random.seed(0)
     idx = np.random.choice(len(train_data), args.n, replace=False)
     Gn = np.array(train_data)[idx]
     labels = labels[idx]
@@ -55,29 +59,43 @@ if __name__ == '__main__':
     Gn = WL_preprocess(Gn)
 
     klf = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
-    scores = Parallel(
-        n_jobs=5)(
-            delayed(fit_predict_one)(
-                Gn,
-                labels,
-                train_index,
-                test_index,
-                c=args.c,
-                h_iter=args.h_iter,
-                edges=args.edges)
-        for train_index, test_index in klf.split(Gn, labels))
+    # scores = Parallel(
+    #     n_jobs=5)(
+    #         delayed(fit_predict_one)(
+    #             Gn,
+    #             labels,
+    #             train_index,
+    #             test_index,
+    #             c=args.c,
+    #             h_iter=args.h_iter,
+    #             edges=args.edges)
+    #     for train_index, test_index in klf.split(Gn, labels))
+
+    N = len(Gn)
+    t0 = time()
+    scores = fit_predict_one(Gn,
+                             labels,
+                             np.arange(N * 4 // 5),
+                             np.arange(N * 4 // 5, N),
+                             c=args.c,
+                             h_iter=args.h_iter,
+                             edges=args.edges)
 
     print("=========================================")
     print("\nAUC score on {:d} samples using WL kernel :  {:.2f} ".format(
         args.n, np.mean(scores)))
+    print("Time : {:.2f} s".format(time() - t0))
     print("\n=========================================")
 
     if args.submit:
+        test_data = pd.read_pickle(data_path / 'test_data.pkl')
         clf = KernelSVC(
             C=args.c,
             kernel=WesifeilerLehmanKernel(h_iter=args.h_iter,
                                           edges=args.edges)
         )
         clf.fit(Gn, labels)
-        y_pred = clf.predict(Gn)
+
+        Gn_test = WL_preprocess(test_data)
+        y_pred = clf.predict(Gn_test)
         pd.DataFrame(y_pred).to_csv('y_pred.csv', index=False)
